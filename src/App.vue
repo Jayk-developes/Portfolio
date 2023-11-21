78321b72c28be364cbccc6220a925b9132895079
joshua.koch <joshua.koch@comnetgmbh.com>
2023-11-21 10:21:18 +0100

    Add style

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,7 @@ document.addEventListener("DOMContentLoaded", () => {
   let wallpaper = document.getElementById("wallpaper")
 
   onscroll = (event) => {
-    const opacity = 1 - (window.pageYOffset / 900)
+    const opacity = 1 - (window.pageYOffset / 1000)
     const scale = 1 + (window.pageYOffset / 5000)
     if (opacity <= 0) {
       wallpaper.style.display = "none"
@@ -21,6 +21,78 @@ document.addEventListener("DOMContentLoaded", () => {
 
 
 })
+
+var overAElement = false
+
+var dots = [],
+    mouse = {
+      x: 0,
+      y: 0
+    };
+
+// The Dot object used to scaffold the dots
+var Dot = function() {
+  this.x = 0;
+  this.y = 0;
+  this.node = (function(){
+    var n = document.createElement("div");
+    n.className = "trail";
+    document.body.appendChild(n);
+    return n;
+  }());
+};
+// The Dot.prototype.draw() method sets the position of
+  // the object's <div> node
+Dot.prototype.draw = function() {
+  this.node.style.left = this.x + "px";
+  this.node.style.top = this.y + "px";
+  this.node.style.boxShadow = overAElement ? "0 0 1.25rem .5rem #ffaa00" : "0 0 1.25rem .5rem white";
+
+
+};
+
+// Creates the Dot objects, populates the dots array
+for (var i = 0; i < 15; i++) {
+  var d = new Dot();
+  dots.push(d);
+}
+
+// This is the screen redraw function
+function draw() {
+  // Make sure the mouse position is set everytime
+    // draw() is called.
+  var x = mouse.x,
+      y = mouse.y;
+
+  // This loop is where all the 90s magic happens
+  dots.forEach(function(dot, index, dots) {
+    var nextDot = dots[index + 1] || dots[0];
+
+    dot.x = x;
+    dot.y = y;
+    dot.draw();
+    x += (nextDot.x - dot.x) * .4;
+    y += (nextDot.y - dot.y) * .4;
+
+  });
+}
+
+addEventListener("mousemove", function(event) {
+  //event.preventDefault();
+  overAElement = event.target.tagName == "A"
+  mouse.x = event.pageX;
+  mouse.y = event.pageY;
+});
+
+// animate() calls draw() then recursively calls itself
+  // everytime the screen repaints via requestAnimationFrame().
+function animate() {
+  draw();
+  requestAnimationFrame(animate);
+}
+
+// And get it started by calling animate().
+animate();
 </script>
 
 <template>
--- src/components/Content.vue
+++ src/components/Content.vue
@@ -7,37 +7,50 @@ document.addEventListener("DOMContentLoaded", () => {
   const personalData = document.getElementById("personalData") as HTMLElement
   const cv = document.getElementById("cv") as HTMLElement
   const cv_header = document.getElementById("cv_header") as HTMLElement
+  const joshua_cv_separator = document.getElementById("joshua_cv_separator") as HTMLElement
+  const data_cv_separator = document.getElementById("data_cv_separator") as HTMLElement
+  const skills_header = document.getElementById("skills_header") as HTMLElement
+  const cv_skills_separator = document.getElementById("cv_skills_separator") as HTMLElement
+  const data_skills_separator = document.getElementById("data_skills_separator") as HTMLElement
+  const skills = document.getElementById("skills") as HTMLElement
+  const contact_header = document.getElementById("contact_header") as HTMLElement
 
-
-  const stretchIfOdd = (element: HTMLElement) => {
-    if (element.childNodes.length % 2 != 0) {
-      console.log(element.childNodes[element.childNodes.length - 1])
-
-      element.childNodes[element.childNodes.length - 1].style.width = "35rem"
-    }
+  if (joshua.getBoundingClientRect().bottom > 0) {
+    joshua.classList.add("fixed_content")
   }
 
-  stretchIfOdd(personalData)
+  window.scrollBy(0, 1)
 
   const isElementOnScreen = (scrollElement: HTMLElement) => {
     const rect = scrollElement.getBoundingClientRect();
-    return rect.top < window.innerHeight / 2 + 40;
+    return rect.top < window.innerHeight / 2 - 200;
   }
 
-  const isElementAbove = (scrollElement: HTMLElement) => {
+  const isElementAbove = (scrollElement: HTMLElement, maximal: number, minimal: number) => {
     const rect = scrollElement.getBoundingClientRect();
-    return rect.bottom < 70
+    return maximal < rect.bottom && rect.bottom < minimal
   }
 
   addEventListener("scroll", (event) => {
+    console.log(skills_header.getBoundingClientRect().bottom)
+    if(!(isElementAbove(personalData, -100000, -270)))
     scrollAnimation(personalData, joshua, top_joshua_separator, joshua_data_separator, cv_header)
+    if (isElementAbove(personalData, -2100, -270)) {
+      cv_header.style.marginTop = "0rem"
+      scrollAnimation(cv, cv_header, joshua_cv_separator, data_cv_separator, skills_header)
+    }
+    if (isElementAbove(cv, -2000, -270)) {
+      skills_header.style.marginTop = "0rem"
+      scrollAnimation(skills, skills_header, cv_skills_separator, data_skills_separator, contact_header)
+    }
 
   });
 
 
   const scrollAnimation = (targetData: HTMLElement, targetHeader: HTMLElement, separatortop?: HTMLElement, separatorbtm: HTMLElement, lowerHeader?: HTMLElement) => {
     const isOnScreen = isElementOnScreen(targetData)
-    const dataAbove = isElementAbove(targetData)
+    const dataAbove = isElementAbove(targetData, -2100, -270)
+    targetHeader.style.marginTop = "0rem"
     targetData.classList.toggle('scale', isOnScreen);
     targetHeader.classList.toggle('fixed_content', !isOnScreen);
     targetHeader.classList.toggle('scalePointSeven', isOnScreen);
@@ -56,27 +69,27 @@ document.addEventListener("DOMContentLoaded", () => {
 
 <template>
   <div class="center">
-    <div style="margin-bottom:65rem; display: none" id="top_joshua_separator"></div>
-    <div class="center fixed_content" id="joshua">
+    <div style="margin-bottom:85rem; display: none" id="top_joshua_separator"></div>
+    <div class="center" id="joshua">
       <h1 class="xl_text" style="margin: 0 0 -2rem 0;">Joshua-Daniel Koch</h1>
       <h3> Fachinformatiker in Fachrichtung Anwendungsentwicklung</h3>
     </div>
-    <div style="margin-bottom:80rem" id="joshua_data_separator"></div>
+    <div style="margin-bottom:100rem" id="joshua_data_separator"></div>
     <div class="content_grid" id="personalData">
       <div id="content_personal">
-       <p> Technik </p>
+        <p> Technik </p>
       </div>
       <div id="content_personal">
-       <p> Digitale Trends </p>
+        <p> Digitale Trends </p>
       </div>
       <div id="content_personal">
-       <p> Erste Erfahrung mit 18 </p>
+        <p> Erste Erfahrung mit 18 </p>
       </div>
       <div id="content_personal">
-       <p> Gewissenhafte Arbeit </p>
+        <p> Gewissenhafte Arbeit </p>
       </div>
       <div id="content_personal">
-       <p> Entwicklung und Programmieren </p>
+        <p> Entwicklung und Programmieren </p>
       </div>
     </div>
     <!--    <p>Ich war schon von Kind aus interessiert an Technik und Digitale Trends.-->
@@ -84,49 +97,66 @@ document.addEventListener("DOMContentLoaded", () => {
     <!--      Obwohl ich vergleichsweise Spät mit dem Entwickeln angefangen habe, kann ich mit meiner Gewissenhaften Arbeit-->
     <!--      überzeugen.</p>-->
 
-
-    <div class=" content_border" id="cv_header">
-      <h1 class="mid_text">Lebenslauf</h1>
+    <div style="margin-bottom:85rem; display: none" id="joshua_cv_separator"></div>
+    <div class="center" id="cv_header">
+      <h1 class="xl_text">Lebenslauf</h1>
     </div>
 
-    <div style="margin-bottom:80rem" id="data_cvseparator"></div>
+    <div style="margin-bottom:100rem" id="data_cv_separator"></div>
     <div class="content_grid" id="cv">
       <div>
-        Fachhochschulreife
+        <p>Fachhoch- schulreife</p>
       </div>
       <div>
-        Engagement
+        <p>Engagement</p>
       </div>
       <div>
-        Motivation
+        <p>Motivation</p>
       </div>
       <div>
-        Notendurchschnitt 1.6
+        <p>Durchschnitt ⌀1.6</p>
       </div>
 
     </div>
-    <h3>Lebenslauf</h3>
-    <p>Ich Konnte meine Schullaufbahn mit Bravur bestehen und habe immer Engagement und Motivation gezeigt.
-      So habe ich mein Erweiterten Sekundarabschluss I mit einem Notendurchschnitt von 1.6 bestehen können. </p>
+    <!--    <h3>Lebenslauf</h3>-->
+    <!--    <p>Ich Konnte meine Schullaufbahn mit Bravur bestehen und habe immer Engagement und Motivation gezeigt.-->
+    <!--      So habe ich mein Erweiterten Sekundarabschluss I mit einem Notendurchschnitt von 1.6 bestehen können. </p>-->
 
-
-    <div class=" content_border">
-      <h1 class="mid_text">Skills</h1>
+    <div style="margin-bottom:85rem; display: none" id="cv_skills_separator"></div>
+    <div class="center" id="skills_header">
+      <h1 class="xl_text">Skills</h1>
     </div>
-    <div class="content_grid">
-      <div>HTML / CSS</div>
-      <div>Javascript / Typescript</div>
-      <div>Vue.js</div>
-      <div>Django</div>
-      <div>FastAPI</div>
-      <div>SQL</div>
+    <div style="margin-bottom:100rem" id="data_skills_separator"></div>
+    <div class="content_grid" id="skills">
+      <div>
+        <p> HTML / CSS </p>
+      </div>
+      <div>
+        <p> Javascript / Typescript </p>
+      </div>
+      <div>
+        <p> Vue.js </p>
+      </div>
+      <div>
+        <p> Django </p>
+      </div>
+      <div>
+        <p> FastAPI </p>
+      </div>
+      <div>
+        <p> SQL </p>
+      </div>
     </div>
-    <h3>Skills</h3>
-    <p>Ich habe mich Spezialisiert in der Entwicklung an Websites, sodass ich nicht nur umfangreiche Erfahrung in
-      HTML, CSS und Javascript/Typescript besitze, sondern auch mit Frameworks wie Vue.js und Django arbeite.</p>
-    <p>Für Backendaufgaben bin ich zusätzlich offen, da ich auch in diesem Bereich mit FastAPI, Django und SQL
-      Daten in Websites angebunden habe.</p>
+    <!--    <h3>Skills</h3>-->
+    <!--    <p>Ich habe mich Spezialisiert in der Entwicklung an Websites, sodass ich nicht nur umfangreiche Erfahrung in-->
+    <!--      HTML, CSS und Javascript/Typescript besitze, sondern auch mit Frameworks wie Vue.js und Django arbeite.</p>-->
+    <!--    <p>Für Backendaufgaben bin ich zusätzlich offen, da ich auch in diesem Bereich mit FastAPI, Django und SQL-->
+    <!--      Daten in Websites angebunden habe.</p>-->
+    <div class="center" id="contact_header">
+      <h1 class="xl_text">Kontakt</h1>
+    </div>
   </div>
+
 </template>
 
 <style scoped>
